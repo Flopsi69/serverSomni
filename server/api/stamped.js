23488c6e993241e41ed3cb305d1873d99b619d01
Alexander Lytovchenko <flopsi69@gmail.com>
2023-03-04 01:26:15 +0200

    fix

--- server/api/stamped.js
+++ server/api/stamped.js
@@ -1,4 +1,12 @@
 export default defineEventHandler(async (event) => {
+  setResponseHeaders(event, {
+    'Access-Control-Allow-Methods': 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Credentials': 'true',
+    'Access-Control-Allow-Headers': '*',
+    'Access-Control-Expose-Headers': '*',
+  });
+
   const { apiShopifySecret, apiShopifyUrl } = useRuntimeConfig();
 
   const { customer_id } = getQuery(event);
